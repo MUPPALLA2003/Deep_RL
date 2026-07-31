953ac32600282f0b8cbc06e3fd84e3f74742425b
MUPPALLA2003 <muppallakomaliitkgp2022@kgpian.iitkgp.ac.in>
2026-07-31 21:04:37 +0530

    game plotting

---
+++ utils/game_plot.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import torch
+import gymnasium as gym
+
+def record_game(
+    env: gym.Env,
+    agent,
+    prefix: str = "agent",
+    video_dir: str = "savevideos",
+    seed: int = 42,
+):
+
+    Path(video_dir).mkdir(parents=True, exist_ok=True)
+
+    env = gym.wrappers.RecordVideo(
+        env,
+        video_folder=video_dir,
+        name_prefix=prefix,
+        disable_logger=True,
+    )
+
+    state, _ = env.reset(seed=seed)
+    done = False
+
+    while not done:
+
+        state_tensor = torch.as_tensor(state,dtype=torch.float32).unsqueeze(0)
+        action = agent.inference(state_tensor)
+        state, _, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
+
+    env.close()
+
+    return Path(video_dir) / f"{prefix}-episode-0.mp4"
